277cb453263ab3ddb958d2718951cc54736f3d6f
Claudiaam18 <158302256+Claudiaam18@users.noreply.github.com>
2025-03-25 07:39:23 -0600

    Ejercicio 1

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -3,6 +3,20 @@ package ed.lab;
 public class E01FlattenBT {
 
     public void flatten(TreeNode<Integer> root) {
+        if (root == null)
+            return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        //indicando los subárboles izquierdos y derechos
+        TreeNode<Integer> leftSubtree = root.left;
+        TreeNode<Integer> rightSubtree = root.right;
+
+        //cambia el subárbol izquierdo hacia el derecho
+        root.left = null;
+        root.right = leftSubtree;
+
 
     }
 
